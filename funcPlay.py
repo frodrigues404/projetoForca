992bbb5974d40f877bc4fd1c0194ff39df1d21a4
Fernando Rodrigues Junior <57274349+frodrigues404@users.noreply.github.com>
2022-10-27 21:28:51 -0300

    Comentários
    Comentários

--- funcPlay.py
+++ funcPlay.py
@@ -5,10 +5,10 @@ from format import *
 def play():
     clear()
     print('===' * 10)
-    player1 = str(input('Digite o nome do jogador 1: ')).strip().capitalize()
-    player2 = str(input('Digite o nome do jogador 2: ')).strip().capitalize()
+    player1 = str(input('Digite o nome do jogador 1: ')).strip().capitalize() #strip() remove espaçamento desnecessário antes e depois da string
+    player2 = str(input('Digite o nome do jogador 2: ')).strip().capitalize() #capitalize() formata o primeiro caractere da string para upper
     print('-' * 10)
-    palavraSecreta = str(input(f'{player1}, Digite a palavra secreta: ')).lower().strip()
+    palavraSecreta = str(input(f'{player1}, Digite a palavra secreta: ')).lower().strip() #lower() formata a string para letras minúsculas
     letrasDescobertas = []
     print('-' * 10)
     try:
